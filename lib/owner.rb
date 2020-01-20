9acc0ca3de0a9204e0e7b61f9e5fa8d7348f9797
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-20 19:24:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,9 +3,9 @@ class Owner
   
   @@all = []
   
-  def initialize(name, species)
+  def initialize(name)
     @name = name 
-    @species = species 
+    @species = human 
     @@all << self
   end
   
