dfb69388ce45f664d85b593bf917f3160dddb751
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-20 19:22:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name, species)
+    @name = name 
+    @species = species 
+    @@all << self
+  end
+  
+  def say_species 
+    puts self.species 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.count 
+    self.all.length 
+  end
+  
+  def self.reset_all 
+    @@all.clear 
+  end
 end
